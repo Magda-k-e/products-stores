f79aabaae989d98fe2f223829ec903509f60915b
Magdalini Kentroti <magda.kentr@gmail.com>
2024-02-14 13:34:53 +0200

    get product category

--- src/main/java/com/products/productsstoresapp/controller/ProductCategoryController.java
+++ src/main/java/com/products/productsstoresapp/controller/ProductCategoryController.java
@@ -5,11 +5,10 @@ import com.products.productsstoresapp.service.ProductCategoryService;
 import com.products.productsstoresapp.transfer.resource.ProductCategoryResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.products.productsstoresapp.model.ProductCategory;
+
+import java.util.List;
 
 @RestController
 public class ProductCategoryController {
@@ -25,6 +24,35 @@ public class ProductCategoryController {
         ProductCategory productCategory = ProductCategoryMapper.INSTANCE.toDomain(productCategoryResource);
         productCategoryService.addProductCategory(productCategory);
         return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    //get all product categories
+    @GetMapping("/productcategories")
+    public ResponseEntity<List<ProductCategoryResource>> getAllProductCategories(){
+        List<ProductCategory> productCategories = productCategoryService.getAllProductCategories();
+        if (!productCategories.isEmpty()){
+            List<ProductCategoryResource> productCategoryResources = productCategories.stream()
+                    .map(ProductCategoryMapper.INSTANCE::toResource)
+                    .toList();
+            return new ResponseEntity<>(productCategoryResources,HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    //    // get category by description
+    @RequestMapping("/productcategories/{description}")
+    public ResponseEntity<List<ProductCategoryResource>> getProductCategory(@PathVariable String description){
+        List<ProductCategory> productCategories = productCategoryService.findByDescription(description);
+        if (!productCategories.isEmpty()) {
+            List<ProductCategoryResource> productCategoryResources = productCategories.stream()
+                    .map(ProductCategoryMapper.INSTANCE::toResource)
+                    .toList();
+            return new ResponseEntity<>(productCategoryResources, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
     }
 
 }
