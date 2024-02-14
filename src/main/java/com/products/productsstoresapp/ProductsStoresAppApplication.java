688ad01afb6062176579c77ecb5592b1c2017740
Magdalini Kentroti <magda.kentr@gmail.com>
2024-02-14 12:26:23 +0200

    first commit

---
+++ src/main/java/com/products/productsstoresapp/ProductsStoresAppApplication.java
@@ -0,0 +1,13 @@
+package com.products.productsstoresapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsStoresAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductsStoresAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/products/productsstoresapp/ProductsStoresAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.products.productsstoresapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsStoresAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
