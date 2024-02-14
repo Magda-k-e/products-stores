3c07f264a36522e86ef1f0f675a2f8dc10c97300
Magdalini Kentroti <magda.kentr@gmail.com>
2024-02-14 16:18:23 +0200

    store and category entities

---
+++ src/main/java/com/products/productsstoresapp/model/StoreCategory.java
@@ -0,0 +1,27 @@
+package com.products.productsstoresapp.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class StoreCategory implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+    private String description;
+    private String fame;
+    @OneToMany(mappedBy = "storeCategory")
+    @JsonIgnore
+    private List<Store> stores;
+}
